75dccdfbb8f7a5a48004bce2c93b6341074e32c7
mzhtv00 <muhammedmecit11@gmail.com>
2026-07-24 17:12:48 +0300

    Update isp_main.py

--- isp_main.py
+++ isp_main.py
@@ -3,6 +3,27 @@ from db_class import MySqlDB
 db = MySqlDB()
 db.connect()
 
+while True:
+    print("\n1. Kullanıcı Ekle\n2. Çıkış\n")
+    choice = int(input("İşlem seçin: "))
+    if (choice == 1):
+        ad_soyad = input("Ad Soyad: ").strip()
+        mail = input("Mail: ").strip()
+        telefon = input("Telefon: ").strip()
+        adres = input("Adres: ")
+        if not telefon.isdigit():
+            print("Lütfen geçerli bir numara girin.")
+            continue
+        kullanici_verileri = {"ad_soyad": ad_soyad, "mail": mail, "telefon": telefon, "adres": adres}
+        db.insert_record("kullanicilar", kullanici_verileri)
+
+    elif (choice == 2):
+        print("Çıkış yapılıyor..")
+        break
+
+    else:
+        print("Geçersiz giriş.")
+
 db.db_olustur("impulsedb")
 db.tablo_olustur("impulsedb", "kullanicilar", 
                  """id INT AUTO_INCREMENT PRIMARY KEY,
@@ -20,19 +41,11 @@ db.tablo_olustur("impulsedb", "tarifeler",
                  ücret_azn INT
                  """)
 
-tarifeler = [
-    ("Standart", 50, 25),
-    ("Premium", 200, 50),
-    ("Business", 500, 90),
-    ("Ultra", 1000, 150)
-]
-#db.tarife_ekle(tarifeler)
-
-kullanici = [
-    ("Abone1", "abone100@gmail.com", "+90 555 444 33 22", "Kocaeli, İzmit", "Standart"),
-    ("Abone2", "abone200@gmail.com", "+90 555 222 33 44", "Bursa, Nilüfer", "Premium")
-]
-db.kullanici_ekle(kullanici)
-
+#tarifeler = [
+#    ("Standart", 50, 25),
+#    ("Premium", 200, 50),
+#    ("Business", 500, 90),
+#    ("Ultra", 1000, 150)
+#]
 
 db.disconnect()
