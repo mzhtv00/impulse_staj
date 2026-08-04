7e0b3791f135fe091bc9c8c7ba579c6c65478bea
mzhtv00 <muhammedmecit11@gmail.com>
2026-08-04 14:12:56 +0300

    Add files via upload

---
+++ dns_analiz.py
@@ -0,0 +1,22 @@
+import dns.resolver
+
+def dns_analizi(domain):
+    print("     DNS KAYIT ANALİZİ")
+    kayit_turleri = ["A", "MX", "NS", "TXT"]
+    for kayit in kayit_turleri:
+        print(f"\n{kayit} Kayıtları:")
+        try:
+            yanitlar = dns. resolver.resolve(domain, kayit)
+            for yanit in yanitlar:
+                print(f"- {yanit.to_text()}")
+        except dns.resolver.NoAnswer:
+            print("Kayıt Bulunamadı")
+        except dns.resolver.NXDOMAIN:
+            print("Domain Bulunamadı")
+            break
+        except Exception as e:
+            print(f"Hata: {e}")
+
+if __name__ == "__main__":
+    hedef = input("Hedef Domain: ").strip()
+    dns_analizi(hedef)
